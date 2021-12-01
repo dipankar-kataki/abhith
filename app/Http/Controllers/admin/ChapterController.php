5efefad4438cb77ee854656a7b7b75042366ee74
DipankarKataki <Dipankar.Kataki@ekodus.com>
2021-12-01 10:34:48 +0530

    chapter creation display order by desc added

--- app/Http/Controllers/admin/ChapterController.php
+++ app/Http/Controllers/admin/ChapterController.php
@@ -13,7 +13,7 @@ class ChapterController extends Controller
     protected function index($id){
         $course_id = \Crypt::decrypt($id);
 
-        $chapters = Chapter::where('course_id',$course_id)->orderBy('created_at','DESC')->simplePaginate(5);
+        $chapters = Chapter::where('course_id',$course_id)->orderBy('id','DESC')->simplePaginate(5);
 
         return view('admin.chapter.chapter', \compact('course_id', 'chapters'));
     }
